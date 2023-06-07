0a7464a7fe3eb86f4f6bdc6660f63fbad8eecfed
Gustavo-Jezini <Gustavojezini@gmail.com>
2023-06-07 10:23:03 -0300

    fix: altera sistema de pastas para branchs

---
+++ prisma/migrations/20230607122828_adiciona_valor_padrao_para_created_at/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "check_ins" ALTER COLUMN "created_At" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "users" ALTER COLUMN "created_at" SET DEFAULT CURRENT_TIMESTAMP;
