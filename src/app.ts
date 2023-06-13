38c26465c5601db38e4ea838754d3304ba633442
Gustavo-Jezini <Gustavojezini@gmail.com>
2023-06-13 16:00:00 -0300

    add: vitest

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,25 @@
 import fastify from 'fastify'
 import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(409).send({
+      message: 'Validation error.',
+      issue: error.format(),
+    })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Realizar um log para tratar melhor o erro
+  }
+
+  return reply.status(500).send({ message: 'Internal Server Error.' })
+})
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,7 @@
-import { prisma } from '@/lib/prisma'
+import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+// import { prisma } from '@/lib/prisma'
 // import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
 // Agora, a instancia será passada pelo construtor
 
@@ -12,19 +14,15 @@ interface RegisterUseCaseRequest {
 // SOLID -> D === Inversão de dependencia
 
 export class RegisterUseCase {
-  constructor(private usersRepository: any) {}
+  constructor(private usersRepository: UsersRepository) {}
 
   async execute({ name, email, password }: RegisterUseCaseRequest) {
     const password_hash = await hash(password, 6)
 
-    const userWithSameEmail = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    })
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
-      throw new Error('E-mail already exists.')
+      throw new UserAlreadyExistsError()
     }
 
     // const prismaUsersRepository = new PrismaUsersRepository()
