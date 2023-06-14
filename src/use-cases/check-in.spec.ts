37ea481adc96f3644ef0f1d52fb22c97ac78a34d
Gustavo-Jezini <Gustavojezini@gmail.com>
2023-06-14 13:37:12 -0300

    checkin

---
+++ src/use-cases/check-in.spec.ts
@@ -0,0 +1,24 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { CheckInUseCase } from './check-in'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let sut: CheckInUseCase
+
+describe('Check In Use Case', () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new CheckInUseCase(checkInsRepository)
+  })
+
+  it('should be able to check in', async () => {
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-id',
+      userId: 'user-id',
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+    expect(checkIn.gym_id).toEqual('gym-id')
+    expect(checkIn.user_id).toEqual('user-id')
+  })
+})
