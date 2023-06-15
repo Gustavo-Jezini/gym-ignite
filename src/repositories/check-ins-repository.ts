7944b658e1b878c356cd68551a33525da819de82
Gustavo-Jezini <Gustavojezini@gmail.com>
2023-06-15 15:10:01 -0300

    add: validate check in

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -1,8 +1,15 @@
 import { Prisma, CheckIn } from '@prisma/client'
 
 export interface CheckInsRepository {
+  findById(id: string): Promise<CheckIn | null>
+
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
+
+  save(checkIn: CheckIn): Promise<CheckIn>
+
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
+
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
+
   countByUserId(userId: string): Promise<number>
 }
---
+++ src/use-cases/validate-check-in.spec.ts
@@ -0,0 +1,40 @@
+import { describe, it, expect, beforeEach, afterEach, vi } from 'vitest'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { ValidateCheckInsUseCase } from './validate-check-in'
+import { ResourceNotFoundError } from './errors/resource-not-found'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let sut: ValidateCheckInsUseCase
+
+describe('Check In Use Case', () => {
+  beforeEach(async () => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new ValidateCheckInsUseCase(checkInsRepository)
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  it('should be able to validate check in', async () => {
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    await sut.execute({
+      checkInId: createdCheckIn.id,
+    })
+
+    expect(createdCheckIn.validated_at).toEqual(expect.any(Date))
+    expect(checkInsRepository.items[0].validated_at).toEqual(expect.any(Date))
+  })
+
+  it('should not be able to validate an inexistent check-in', async () => {
+    await expect(() =>
+      sut.execute({ checkInId: 'inexistent-check-in-id' }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
