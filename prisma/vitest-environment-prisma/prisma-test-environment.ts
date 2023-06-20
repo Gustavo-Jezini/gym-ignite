8ed8b84b1c43386e17e494684789acb9f429d243
Gustavo-Jezini <Gustavojezini@gmail.com>
2023-06-20 20:58:22 -0300

    create: test e2e

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,15 +1,46 @@
+import 'dotenv/config'
+import { randomUUID } from 'crypto'
 import { Environment } from 'vitest'
+import { execSync } from 'child_process'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+// "postgresql://docker:docker@localhost:5432/apisolid?schema=public" -> a função altera o valor de schema
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 // Aqui é criado um ambiente isolado para o teste E2E
 export default <Environment>{
   name: 'prisma',
   // Essa função roda antes de cada ARQUIVO de teste
   async setup() {
-    console.log('Executou')
+    const schema = randomUUID()
+    const databaseurl = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = databaseurl
+
+    execSync('npx prisma migrate deploy')
 
     return {
       // Isso roda ao final de todos os testes do ARQUIVO
-      teardown() {},
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+
+        await prisma.$disconnect()
+      },
     }
   },
 }
--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,22 @@
-import { test } from 'vitest'
+import { app } from '@/app'
+import request from 'supertest'
+import { describe, expect, it, beforeAll, afterAll } from 'vitest'
 
-test('', () => {})
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
